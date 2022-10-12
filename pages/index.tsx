155e7a56824f9fdb7bf7b42df376c73c7a456903
Al-Amin Firdows <alaminfirdows@gmail.com>
2022-10-12 12:56:49 +0600

    refactor: replace react by nextjs and add tailwind css

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next';
+
+const Home: NextPage = () => {
+	return (
+		<div className='py-12'>
+			<div className='grid place-content-center'>
+				ai-song-writer-client
+			</div>
+		</div>
+	);
+};
+
+export default Home;
